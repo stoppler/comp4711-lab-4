3c1a8b4bca1552b5dd85896e5dd6e0c8e0f14d49
Instructor, Computer Systems Technology <jim_parry@bcit.ca>
2016-09-29 15:21:39 -0700

    Update justone.php

--- application/views/justone.php
+++ application/views/justone.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="span3"><img src="/data/{mug}" title="{who}"/></div>
+    <div class="span3"><img src="/pix/{mug}" title="{who}"/></div>
     <div class="span8 offset1">
         <p class="lead">{what}</p><br/>
         <p class="text-right">{who}</p>
